ebc26547853035566cce58892c4c9261fd4894c5
Frances Tran <frances.tran1600@gmail.com>
2023-07-18 15:33:59 -0500

    start of home page

--- CATX/ContentView.swift
+++ CATX/ContentView.swift
@@ -8,26 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showNextPage = false
+    
     var body: some View {
         NavigationStack {
             VStack {
-                Text("Welcome to CATX!")
-                    .font(.headline)
-                    .fontWeight(.heavy)
-                    .toolbar {
-                        NavigationLink(destination: SecondView()) {
-                            Text("Second View")
-                        }
-                    }
-            }
-            
-        }
-        .navigationTitle("CATX")
-    }
-}
+                Text("What's Tossin'")
+                    .font(.title)
+                    .fontWeight(.bold)
+                
+                Button(action: { showNextPage = true }) {
+                label: do {
+                    Image("aTX2")
+                }
+                   
+                    
+                
+                            NavigationLink("", destination:  SecondView(), isActive: $showNextPage)
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+                            }
+                Text("click on Austin to start")
+                    .foregroundColor(Color.blue)
+                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                
+                Text("In Austin?")
+                    .font(.title)
+                    .fontWeight(.bold)
+                        }
+                }
+                
+            }
+           
+        }
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
-}
+
