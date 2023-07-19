f1539986a55f7a84097490f8facf0941608ab7e5
Ana Kaur <anahita.kaur@gmail.com>
2023-07-19 09:52:03 -0500

    changes

--- CATX/SecondView.swift
+++ CATX/SecondView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct SecondView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        Image("AustinMap")
+        VStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Image("AustinMap")
+            
+            
+            
+        }
+        
     }
 }
 
